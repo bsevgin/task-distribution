9448fc81fc5c418385fb662a4b5f53ec507126e1
bsevgin <bunyaminsevgin@gmail.com>
2023-04-02 18:49:46 +0300

    created migrations, models and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Developer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $developers = array_map(function ($item) {
+            return [
+                'name' => 'DEV' . $item,
+                'difficulty_rate' => $item
+            ];
+        }, range(1, 5));
+
+        Developer::insert($developers);
     }
 }
